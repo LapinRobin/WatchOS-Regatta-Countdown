401c4a90c3aff7bcab242df8194603303d356d1d
LapinRobin <61903127+LapinRobin@users.noreply.github.com>
2023-02-10 19:03:01 +0100

    Initial Commit

---
+++ Regatta-Countdown Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Regatta-Countdown Watch App
+//
+//  Created by Su Yikang on 2023/2/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Regatta-Countdown Watch App/Regatta_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Regatta_CountdownApp.swift
+//  Regatta-Countdown Watch App
+//
+//  Created by Su Yikang on 2023/2/10.
+//
+
+import SwiftUI
+
+@main
+struct Regatta_Countdown_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

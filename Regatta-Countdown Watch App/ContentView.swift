e04052a7c793278506f0fb35275da4528fdd98c7
LapinRobin <61903127+LapinRobin@users.noreply.github.com>
2023-02-18 23:52:46 +0100

    Incomplete implementation of start, sync, and restart functionalities.

--- Regatta-Countdown Watch App/ContentView.swift
+++ Regatta-Countdown Watch App/ContentView.swift
@@ -9,41 +9,56 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var timeRemaining = 60
-    @State private var isCountingDown = false
+    @State private var minutesRemaining = 5
+    @State private var secondsRemaining = 60
     @State private var millisecondsRemaining = 0
+    @State private var isReady = true
+    @State private var isCountingDown = false
+    @State private var isTimeUp = false
+    
+    @State private var buttonText = "Start"
     private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
 
     var body: some View {
         VStack {
             HStack{
-                
-                Text("\(timeRemaining / 10)")
+                Text("\(minutesRemaining)")
                   .font(.largeTitle)
                   .frame(width: 22)
-                Text("\(timeRemaining % 10)")
+                Text(":")
                   .font(.largeTitle)
-                  .frame(width: 22)
+                  
+                Text("\(secondsRemaining / 10)")
+                    .font(.system(size: 66))
+                  .frame(width: 36)
+                Text("\(secondsRemaining % 10)")
+                    .font(.system(size: 66))
+                  .frame(width: 36)
                 Text(",")
-                  .font(.largeTitle)
+                    .font(.largeTitle)
                 Text("\(String(format: "%01d", millisecondsRemaining / 10))")
-                  .font(.largeTitle)
-                  .frame(width: 22)
+                    .font(.system(size: 20))
+                    .alignmentGuide(.bottom) { d in d[.bottom] }
+                    .frame(width: 11)
                 
                 Text("\(String(format: "%01d", millisecondsRemaining % 10))")
-                  .font(.largeTitle)
-                  .frame(width: 22)
+                  .font(.system(size: 20))
+                  .alignmentGuide(.bottom) { d in d[.bottom] }
+                  .frame(width: 11)
             }
             
             // .fixedSize(horizontal: true, vertical: false)
             //.frame(minWidth: 40, idealWidth: 40, maxWidth: 40, alignment: .center)
 
                 .onReceive(timer) { _ in
-                  if self.timeRemaining > 0 && self.isCountingDown {
+                  if self.secondsRemaining > 0 && self.isCountingDown {
                       if self.millisecondsRemaining == 0 {
-                          self.timeRemaining -= 1
-                          if self.timeRemaining == 0 {
+                          self.secondsRemaining -= 1
+                          if self.secondsRemaining == 0 {
                               self.millisecondsRemaining = 0
+                              self.isCountingDown = false
+                              self.isTimeUp = true
+                              buttonText = "Reset"
                           } else {
                               self.millisecondsRemaining = 99
                           }
@@ -56,17 +71,27 @@ struct ContentView: View {
              
                 
               Button(action: {
-                self.isCountingDown.toggle()
-                if self.isCountingDown {
-                    self.timeRemaining = 60
-                    self.millisecondsRemaining = 0
-                }
+                  if self.isReady {
+                      self.isReady.toggle()
+                      self.isCountingDown.toggle()
+                      buttonText = "Sync"
+                  } else if self.isCountingDown {
+                      if secondsRemaining > 5 {
+                          self.secondsRemaining -= 5
+                      }
+                      
+                  } else {
+                      buttonText = "Start"
+                      self.secondsRemaining = 60
+                      self.isReady = true
+                  }
               }) {
-                Text(isCountingDown ? "Stop" : "Start")
+                Text(buttonText)
               }
                 
         }
     }
+    
 }
 
 

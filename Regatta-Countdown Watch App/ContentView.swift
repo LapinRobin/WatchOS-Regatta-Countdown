ac50eec6ed0b9623c98db3b7eb6462ecc382294e
LapinRobin <61903127+LapinRobin@users.noreply.github.com>
2023-03-04 20:08:58 +0100

    Basic functionalities added and tested

--- Regatta-Countdown Watch App/ContentView.swift
+++ Regatta-Countdown Watch App/ContentView.swift
@@ -10,85 +10,88 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var minutesRemaining = 5
-    @State private var secondsRemaining = 60
-    @State private var millisecondsRemaining = 0
+    @State private var secondsRemaining = 0
+    @State private var decisecondsRemaining = 0
     @State private var isReady = true
     @State private var isCountingDown = false
     @State private var isTimeUp = false
     
     @State private var buttonText = "Start"
-    private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
-
+    private let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
+    func triggerHaptic(type: WKHapticType) {
+        let interfaceDevice = WKInterfaceDevice.current()
+        interfaceDevice.play(type)
+    }
+    
     var body: some View {
         VStack {
             HStack{
                 Text("\(minutesRemaining)")
-                  .font(.largeTitle)
-                  .frame(width: 22)
+                    .font(.largeTitle)
+                    .frame(width: 22)
                 Text(":")
-                  .font(.largeTitle)
-                  
+                    .font(.largeTitle)
+                
                 Text("\(secondsRemaining / 10)")
                     .font(.system(size: 66))
-                  .frame(width: 36)
+                    .frame(width: 36)
                 Text("\(secondsRemaining % 10)")
                     .font(.system(size: 66))
-                  .frame(width: 36)
+                    .frame(width: 36)
                 Text(",")
                     .font(.largeTitle)
-                Text("\(String(format: "%01d", millisecondsRemaining / 10))")
-                    .font(.system(size: 20))
-                    .alignmentGuide(.bottom) { d in d[.bottom] }
-                    .frame(width: 11)
                 
-                Text("\(String(format: "%01d", millisecondsRemaining % 10))")
-                  .font(.system(size: 20))
-                  .alignmentGuide(.bottom) { d in d[.bottom] }
-                  .frame(width: 11)
+                Text("\(String(format: "%01d", decisecondsRemaining % 10))")
+                    .font(.largeTitle)
+                    .frame(width: 22)
             }
             
             // .fixedSize(horizontal: true, vertical: false)
             //.frame(minWidth: 40, idealWidth: 40, maxWidth: 40, alignment: .center)
-
-                .onReceive(timer) { _ in
-                  if self.secondsRemaining > 0 && self.isCountingDown {
-                      if self.millisecondsRemaining == 0 {
-                          self.secondsRemaining -= 1
-                          if self.secondsRemaining == 0 {
-                              self.millisecondsRemaining = 0
-                              self.isCountingDown = false
-                              self.isTimeUp = true
-                              buttonText = "Reset"
-                          } else {
-                              self.millisecondsRemaining = 99
-                          }
-                        
-                      } else {
-                        self.millisecondsRemaining -= 1
-                      }
-                  }
+            
+            .onReceive(timer) { _ in
+                if self.isCountingDown {
+                    if self.minutesRemaining == 0 && self.secondsRemaining == 0 && self.decisecondsRemaining == 0 {
+                        triggerHaptic(type: .success)
+                        self.isTimeUp = true
+                        self.isCountingDown = false
+                        buttonText = "Reset"
+                    } else if self.decisecondsRemaining == 0 {
+                        decisecondsRemaining = 9
+                        if (self.secondsRemaining == 0) {
+                            self.secondsRemaining = 59
+                            self.minutesRemaining -= 1
+                        } else {
+                            self.secondsRemaining -= 1
+                        }
+                    } else {
+                        self.decisecondsRemaining -= 1
+                    }
                 }
-             
-                
-              Button(action: {
-                  if self.isReady {
-                      self.isReady.toggle()
-                      self.isCountingDown.toggle()
-                      buttonText = "Sync"
-                  } else if self.isCountingDown {
-                      if secondsRemaining > 5 {
-                          self.secondsRemaining -= 5
-                      }
-                      
-                  } else {
-                      buttonText = "Start"
-                      self.secondsRemaining = 60
-                      self.isReady = true
-                  }
-              }) {
+            }
+            
+            
+            Button(action: {
+                if self.isReady {
+                    triggerHaptic(type: .start)
+                    self.isReady.toggle()
+                    self.isCountingDown.toggle()
+                    buttonText = "Sync"
+                } else if self.isCountingDown {
+                    if minutesRemaining > 0 {
+                        self.secondsRemaining = 0
+                        self.decisecondsRemaining = 0
+                    }
+                    
+                } else {
+                    buttonText = "Start"
+                    self.minutesRemaining = 5
+                    self.isReady = true
+                }
+            }) {
                 Text(buttonText)
-              }
-                
+            }
         }
     }
     
